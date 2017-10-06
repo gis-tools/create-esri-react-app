369bd8df1aa01a7ee8c8023d121abd5bcfd0632c
Bojan Zivkovic <bzivkovic@blueraster.com>
2017-10-05 22:28:57 -0400

    Updating code for v3 of ESRI ArcGIS API

--- build/resources/App3.js
+++ build/resources/App3.js
@@ -2,41 +2,53 @@ import React, {Component} from 'react';
 import './App.css';
 import * as esriLoader from 'esri-loader';
 
-if (!esriLoader.isLoaded()) {
-  esriLoader.bootstrap((err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      createMap();
-    }
-  }, {
-    url: 'https://js.arcgis.com/3.21/' // Here you can change API version
-  });
-} else {
-  createMap();
-}
-
-function createMap() {
-  esriLoader.dojoRequire([
-    'esri/map'
-  ], (Map) => {
-    let map = new Map('mapNode', {
-      center: [-100, 30],
-      zoom: 3,
-      basemap: 'gray-vector'
-    });
-    window.map = map;
-  });
-}
-
 class App extends Component {
+  constructor() {
+    super();
+
+    if (!esriLoader.isLoaded()) {
+      esriLoader.bootstrap((err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          this.createMap();
+        }
+      }, {
+        url: 'https://js.arcgis.com/3.22/' // Here you can change API version
+      });
+    } else {
+      this.createMap();
+    }
+
+    this.state = {
+      map: null
+    }
+  }
+
+  createMap = () => {
+    esriLoader.dojoRequire([
+      'esri/map'
+    ], (Map) => {
+      let map = new Map('mapContainer', {
+        center: [-100, 30],
+        zoom: 3,
+        basemap: 'gray-vector'
+      });
+      window.map = map;
+
+      this.setState({
+        map
+      })
+    });
+  };
+
   render() {
     return (
         <div className="App">
           <div className="App-header">
             <h1>Welcome to ESRI React App</h1>
           </div>
-          <div id="mapNode"/>
+          <div id="mapContainer"/>
         </div>
     );
   }
