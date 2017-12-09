de8250a23b6fc3c5970e161477268dc376261abd
Bojan Zivkovic <bzivkovic@blueraster.com>
2017-12-08 23:45:03 -0500

    Optimizing App.js for API v3 and v4

--- build/resources/App4.js
+++ build/resources/App4.js
@@ -1,54 +1,41 @@
 import React, {Component} from 'react';
-import * as esriLoader from 'esri-loader';
+import esriLoader from 'esri-loader';
 import './App.css';
 
-class App extends Component {
+const options = {
+  url: 'https://js.arcgis.com/4.5'
+};
+
+export default class App extends Component {
   constructor() {
     super();
 
-    if (!esriLoader.isLoaded()) {
-      esriLoader.bootstrap((err) => {
-        if (err) {
+    esriLoader.loadModules([
+      'esri/Map',
+      'esri/views/MapView'
+    ], options)
+        .then(([Map, MapView]) => {
+          let map = new Map({
+            basemap: "gray-vector"
+          });
+
+          let view = new MapView({
+            map: map,
+            container: "mapContainer",
+            basemap: 'gray-vector',
+            center: [-100, 30],
+            zoom: 5
+          });
+
+          this.setState({
+            map,
+            view
+          })
+        })
+        .catch(err => {
           console.error(err);
-        } else {
-          this.createMap();
-        }
-      }, {
-        // url: 'https://js.arcgis.com/4.4/' // Here you can change API version
-      });
-    } else {
-      this.createMap();
-    }
-
-    this.state = {
-      map: null,
-      view: null
-    }
+        });
   }
-
-  createMap = () => {
-    esriLoader.dojoRequire([
-      "esri/Map",
-      "esri/views/MapView"
-    ], (Map, MapView) => {
-      let map = new Map({
-        basemap: "gray-vector"
-      });
-      window.map = map;
-      let view = new MapView({
-        map: map,
-        container: "mapContainer",
-        basemap: 'gray-vector',
-        center: [-100, 30],
-        zoom: 3
-      });
-
-      this.setState({
-        map,
-        view
-      })
-    });
-  };
 
   render() {
     return (
@@ -56,10 +43,8 @@ class App extends Component {
           <div className="App-header">
             <h1>Welcome to ESRI React App</h1>
           </div>
-          <div id="mapContainer" />
+          <div id="mapContainer"/>
         </div>
     );
   }
 }
-
-export default App;
