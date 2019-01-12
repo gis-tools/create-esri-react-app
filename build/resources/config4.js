7552f287b91561305745a909e9cc661a5c279d03
Bojan Zivkovic <bziveo@gmail.com>
2019-01-11 23:46:41 -0500

    Adding config files for ESRI API v3 and v4

---
+++ build/resources/config4.js
@@ -0,0 +1,7 @@
+const esriApiVersion = "4.10";
+
+export const esriCSS = `https://js.arcgis.com/${esriApiVersion}/esri/css/main.css`;
+export const esriJS = `https://js.arcgis.com/${esriApiVersion}`;
+export const esriOptions = {
+  url: esriJS
+};
