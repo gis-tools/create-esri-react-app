0cb5960a7d1e393d8fe299e7bf064d1ca06041d6
Bojan Zivkovic <bziveo@gmail.com>
2019-01-11 23:48:55 -0500

    Rewriting App.js files for ESRI Map v3 and 4

--- build/resources/App4.js
+++ build/resources/App4.js
@@ -1,50 +1,45 @@
 import React, {Component} from 'react';
-import esriLoader from 'esri-loader';
+import {loadModules, loadCss} from 'esri-loader';
+import { esriCSS, esriOptions } from "./config";
 import './App.css';
 
-const options = {
-  url: 'https://js.arcgis.com/4.6'
-};
+loadCss(esriCSS);
 
 export default class App extends Component {
-  constructor() {
-    super();
 
-    esriLoader.loadModules([
+  componentDidMount = () => {
+    loadModules([
       'esri/Map',
       'esri/views/MapView'
-    ], options)
-        .then(([Map, MapView]) => {
-          let map = new Map({
-            basemap: "gray-vector"
-          });
+    ], esriOptions)
+      .then(([Map, MapView]) => {
 
-          let view = new MapView({
-            map: map,
-            container: "mapContainer",
-            basemap: 'gray-vector',
-            center: [-100, 30],
-            zoom: 5
-          });
-
-          this.setState({
-            map,
-            view
-          })
-        })
-        .catch(err => {
-          console.error(err);
+        const map = new Map({
+          basemap: 'gray-vector'
         });
-  }
+
+        const view = new MapView({
+          map: map,
+          container: 'mapContainer',
+          basemap: 'gray-vector',
+          center: [-100, 30],
+          zoom: 5
+        });
+
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
 
   render() {
     return (
-        <div className="App">
-          <div className="App-header">
-            <h1>Welcome to ESRI React App</h1>
-          </div>
-          <div id="mapContainer"/>
+      <div className="App">
+        <div className="App-header">
+          <h1>Welcome to ESRI React App</h1>
         </div>
+        <div id="mapContainer"/>
+      </div>
     );
   }
 }
